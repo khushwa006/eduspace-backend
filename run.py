e32ae2e1413ca58d211b8c9cb76ede373976b824
Khushwant Kumar <khushwant2204@gmail.com>
2026-07-17 11:55:54 +0530

    Fix indentation and formatting in run.py

--- run.py
+++ run.py
@@ -3061,9 +3061,9 @@ def seed_data():
 
 
 with app.app_context():
-       db.create_all()
+    db.create_all()
 
-   if __name__ == '__main__':
-       with app.app_context():
-           seed_data()
-       app.run(debug=True, port=5000)
+if __name__ == '__main__':
+    with app.app_context():
+        seed_data()
+    app.run(debug=True, port=5000)
